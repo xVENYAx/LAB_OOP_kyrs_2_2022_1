850f130cb2c09dd5848f73ef7b71e8e7c0db1338
Sasha <t3t3t3@ukr.net>
2022-01-28 09:35:46 +0200

    Добавьте файлы проекта.

---
+++ START/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace START
+{
+    public partial class Form1 : Form
+    {
+        int count = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login, str, Password, pas = "";
+            Password = textBox2.Text;
+            login = textBox1.Text;
+            if (login == "VENYA")
+            {
+                label2.BackColor = Color.Green;
+                if (Password == "1111")
+                {
+                    str = "Привіт " + login;
+                    label3.BackColor = Color.Green;
+                    MessageBox.Show(str, "Name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    count--;
+                    pas = "Спробуйте ще раз. " +
+                        "У вас залишилося " + count.ToString() + " попитки";
+                    if (count == 0)
+                    {
+                        this.Enabled = false;
+                    }
+                    label3.BackColor = Color.Red;
+                    MessageBox.Show(pas, "Name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+           
+            if (login != "VENYA")
+            {
+                str = "Ви хто?";
+                label2.BackColor = Color.Red;
+                label3.BackColor = Color.Red;
+                MessageBox.Show(str, "Name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+    }
+}
